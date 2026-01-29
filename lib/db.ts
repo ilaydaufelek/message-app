5012b05a97c656f35b07d7a86c8108a289faf0e4
ilaydaufelek <ilaydaufelek@gmail.com>
2026-01-29 14:11:16 +0300

    fix prisma client import for generated client

--- lib/db.ts
+++ lib/db.ts
@@ -1,10 +1,15 @@
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient as PrismaClientExport } from "@/generated/prisma/client";
+
+// 👇 constructor olarak zorla tanıtıyoruz
+const PrismaClient =
+  PrismaClientExport as unknown as new () => PrismaClientExport;
 
 declare global {
-  var prisma: PrismaClient | undefined;
+  var prisma: InstanceType<typeof PrismaClient> | undefined;
 }
 
-export const db = globalThis.prisma || new PrismaClient();
+export const db =
+  globalThis.prisma ?? new PrismaClient();
 
 if (process.env.NODE_ENV !== "production") {
   globalThis.prisma = db;
